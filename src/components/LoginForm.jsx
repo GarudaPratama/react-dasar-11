5778df71c9a0fac79a739bfb6cb70e209f5fad5a
Nama Kamu <email@kamu.com>
2026-08-05 14:00:17 +0700

    PUSH

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function LoginForm() {
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(event.target.elements.password.value, event.target.elements.username.value);
-        
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username.toUpperCase(), password)
     }
-  return (
-    <form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" id="username" />
-        </div>
 
-        <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-        </div>
 
-        <button type='submit'>Kirim</button>
-    </form>
-  )
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='username'>Username</label>
+                <input name='username' type="text" value={username.toUpperCase()} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+
+            <div>
+                <label htmlFor='password'>Password</label>
+                <input name='password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>    
+
+            <button 
+                style={{display:' block'}}
+                type='submit'
+            >
+                Kirim
+            </button>
+        </form>
+    )
 }
 
